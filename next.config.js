8dcbb5244c445fc0a5dd5efa30d389fe5b18e7ba
Zhang Zehua <edward.zehua.zhang@gmail.com>
2025-05-13 14:25:47 +0700

    js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // TypeScript settings
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
+  // ESLint settings
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
+  // React settings
+  reactStrictMode: true,
+  
+  // Image settings - combined from both configs
   images: {
-    domains: ['github.com'],
+    domains: ['github.com', 'picsum.photos'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,10 +24,26 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'media.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
       }
     ]
   },
-  reactStrictMode: true,
+  
+  // Webpack configuration for SVG support
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
